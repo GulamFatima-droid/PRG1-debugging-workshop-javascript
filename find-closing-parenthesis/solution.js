cca348615c2c7651231d6d63b629a26e456c8ce0
GulamFatima-droid <gulamfatima.ahmadnaz@ada.ac.uk>
2024-09-30 10:41:27 +0000

    bvhgchg

--- find-closing-parenthesis/solution.js
+++ find-closing-parenthesis/solution.js
@@ -1,22 +1,40 @@
-function findClosingParenthesis(str, n) {
-  let count = 0;
-  let i = n;
+// function findClosingParenthesis(str, n) {
+//   let count = 0;
+//   let i = n;
 
-  while (i < str.length) {
-    if (str[i] === "(") {
-      count++;
-    } else if (str[i] === ")") {
-      count--;
+//   while (i < str.length) {
+//     if (str[i] === "(") {
+//       count++;
+//     } else if (str[i] === ")") {
+//       count--;
+//     }
+
+//     if (str.length === 0) {
+//       return i;
+//     }
+
+//     i++;
+//   }
+
+//   // return -1;
+// }
+
+
+
+function findClosing(str,n){
+  let brackets = 2;
+  let i = 0;
+  while (brackets !==0){
+    if(str[i] === '('){
+      brackets-=1;
     }
-
-    if (count === 0) {
-      return i;
+    if(brackets ===0){
+      i--;
     }
-
     i++;
   }
 
-  return -1;
+  console.log(i);
 }
 
-console.log(findClosingParenthesis("Hello, (world). (Something Else).", 2)); // should return 31
+console.log(findClosing("Hello, (world). (Something Else).", 2)); // should return 31
